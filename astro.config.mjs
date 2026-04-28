f5675a8875041903be665679733185e290b242cc
Jeewon Kim <gabyjennn@gmail.com>
2026-04-28 12:27:16 -0400

    javascript buttons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
-site: "https://astrojeewon.netlify.app"
+export default defineConfig({
+  site: "https://astrojeewon.netlify.app",
+  integrations: [preact()]
+});
